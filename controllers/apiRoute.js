b0470643aeebb62e77f3e6671068c1c082e885f9
Arvin John Bautista <arvinyoung01@gmail.com>
2021-05-06 17:33:33 -0700

    apiRoutes all done, needs further testing

--- controllers/apiRoute.js
+++ controllers/apiRoute.js
@@ -1,53 +1,51 @@
 const router = require("express").Router();
-const Workout = require("../models/Workout");
+const db = require("../models");
 
-router.get("/api/workouts", (req, res) => {
-  Workout.find({})
-    .then((dbWorkout) => {
-      res.json(dbWorkout);
-    })
-    .catch((err) => {
-      res.json(err);
-    });
+// GET route last workout from db
+router.get("/api/workouts", async (req, res) => {
+  try {
+    const lastWorkout = await db.Workout.find({}).sort({ _id: 1 });
+    res.status(200).json(lastWorkout);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.post("/api/workouts", (req, res) => {
-  Workout.create({})
-    .then((dbWorkout) => {
-      res.json(dbWorkout);
-    })
-    .catch((err) => {
-      res.json(err);
-    });
+// Add exercises to the most recent workout plan.
+router.put("/api/workouts/:id", async (req, res) => {
+  try {
+    const addExercise = await db.Workout.findByIdAndUpdate(
+      req.param.id,
+      { $push: { exercises: req.body } },
+      { new: true, runValidators: true }
+    );
+    res.status(200).json(addExercise);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.put("/api/workouts/:id", (req, res) => {
-  Workout.findByIdAndUpdate(
-    // the id of the item to find
-    req.params.id,
-    // the change to be made. Mongoose will smartly combine your existing
-    // document with this change, which allows for partial updates too
-    { $push: { exercises: req.body } },
-    // an option that asks mongoose to return the updated version
-    // of the document instead of the pre-updated one.
-    { new: true }
-  )
-    .then((dbWorkout) => {
-      res.json(dbWorkout);
-    })
-    .catch((err) => {
-      res.json(err);
-    });
+// Add new exercises to a new workout plan.
+router.post("/api/workouts/", async (req, res) => {
+  try {
+    const newWorkout = await db.Workout.create(req.body);
+    res.status(200).json(newWorkout);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.delete("/api/workouts/:id", (req, res) => {
-  Workout.findByIdAndDelete(req.body.id)
-    .then((dbWorkout) => {
-      res.json(dbWorkout);
-    })
-    .catch((err) => {
-      res.json(err);
-    });
+// GET route certain workouts within a certain range
+router.get("/api/workouts/range", async (req, res) => {
+  try {
+    const workoutsWithinCertainRange = await db.Workout.find({})
+      .limit(7)
+      .sort({ _id: -1 });
+    const newWorkoutsWithinCertainRange = workoutsWithinCertainRange.reverse();
+    res.status(200).json(newWorkoutsWithinCertainRange);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
